3181a727fbf755b58a1b411f9b015138b431da70
Anjalie Kini <askmac@Anjalies-MBP.fios-router.home>
2018-03-14 17:58:52 -0400

    Created tabs, Added 10 issues, added ways to take action for each issues (without actual links, info)

---
+++ Echo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Echo
+//
+//  Created by Anjalie Kini on 3/3/18.
+//  Copyright © 2018 Anjalie Kini. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
